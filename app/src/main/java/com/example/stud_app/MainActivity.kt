7e8ffe24bf78c6d8c8cc4f0b68615751dee7f22e
aspid1488 <alekseev_na_18@mf.grsu.by>
2020-08-05 01:00:45 +0300

    first commit

---
+++ app/src/main/java/com/example/stud_app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.stud_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        BtnShowToast.setOnClickListener{
+            Log.i("MainActivity", "Button ebat")
+
+            Toast.makeText(this, "ButtOn", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
